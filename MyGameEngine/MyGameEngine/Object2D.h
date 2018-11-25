81f18cca6198988c74d03745d9110506549e638b
Sumi <sumilcy22888@live.jp>
2018-11-24 22:33:24 -0700

    delete normal new function

--- MyGameEngine/MyGameEngine/Object2D.h
+++ MyGameEngine/MyGameEngine/Object2D.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Vector2D.h"
 #include "HeapManager.h"
+#include "DebugLog.h"
 #include <assert.h>
 
 class Object2D
@@ -16,15 +17,37 @@ public:
 	Object2D(const Object2D & obj);
 
 	Object2D & operator=(const Object2D & obj);
+
+	void * operator new(size_t) = delete;
+
 	void * operator new(size_t i_size, HeapManager * pHeap) {
 		void * rtnp = reinterpret_cast<void*>(reinterpret_cast<uintptr_t>(pHeap->_alloc(i_size, 4)) + sizeof(Using));
 		assert(pHeap->_alloc(i_size, 4));
 		return rtnp;
 	}
+
+	void * operator new[](size_t i_size, HeapManager * pHeap) {
+		void * rtnp = reinterpret_cast<void*>(reinterpret_cast<uintptr_t>(pHeap->_alloc(i_size, 4)) + sizeof(Using));
+		assert(pHeap->_alloc(i_size, 4));
+		return rtnp;
+	}
+
 	void operator delete(void * iptr) {
 		void * rtnp = reinterpret_cast<void*>(reinterpret_cast<uintptr_t>(iptr) - sizeof(Using));
 		HeapManager * pHeap;
 		pHeap = static_cast<HeapManager *>(iptr);
+		pHeap->_free(rtnp);
+		return;
+	}
+
+	void operator delete(void * iptr, HeapManager * pHeap) {
+		void * rtnp = reinterpret_cast<void*>(reinterpret_cast<uintptr_t>(iptr) - sizeof(Using));
+		pHeap->_free(rtnp);
+		return;
+	}
+
+	void operator delete[](void * iptr, HeapManager * pHeap) {
+		void * rtnp = reinterpret_cast<void*>(reinterpret_cast<uintptr_t>(iptr) - sizeof(Using));
 		pHeap->_free(rtnp);
 		return;
 	}
