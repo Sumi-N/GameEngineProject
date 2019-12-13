3a3e8ffb2d80da8139e18d13a833249736d892c6
Sumi <sumi.n.lcy@gmail.com>
2019-12-13 16:17:57 -0700

    Separate intrinsic instruction for vector instruction

--- MyGameEngine/Core/Math/Vector.h
+++ MyGameEngine/Core/Math/Vector.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "Define.h"
+
 #include <math.h>
 #include <intrin.h>
 #include <xmmintrin.h>
@@ -18,12 +20,16 @@ namespace Math {
 		{
 			struct { T x, y, z;};
 			struct { T r, g, b;};
+			T ele[3];
 		};
 
+		//!@ Constructor
 		Vec3() = default;
 		Vec3(T i_x, T i_y, T i_z) : x(i_x), y(i_y), z(i_z) {}
 
-		template <typename U> explicit Vec3(Vec3<U> const& u) : x(static_cast<T>(u.x)), y(static_cast<T>(u.y)), z(static_cast<T>(u.z)) {}
+		template <typename U> 
+		explicit Vec3(Vec3<U> const& u) : x(static_cast<T>(u.x)), y(static_cast<T>(u.y)), z(static_cast<T>(u.z)) {}
+		explicit Vec3(Vec4<T> const& t) : x(t.x), y(t.y), z(t.z) {}
 
 		//!@ Operator with constant value
 		friend Vec3 operator + (T t, Vec3 const& u) { Vec3 o_v; o_v.x = t + u.x; o_v.y = t + u.y; o_v.z = t + u.z; return o_v; }
@@ -53,7 +59,7 @@ namespace Math {
 		void Zero      ()       { x = 0; y = 0; z = 0; }
 		bool IsZero    () const { return x == static_cast<T>(0) && y == static_cast<T>(0) && z == static_cast<T>(0); }
 		void Normalize ()       { *this /= Length(); }
-		T    Length    () const { return sqrt(static_cast<T>(x)* static_cast<T>(x) + static_cast<T>(y)* static_cast<T>(y) + static_cast<T>(z)* static_cast<T>(z)); }
+		T    Length    () const { return Sqrt(static_cast<T>(x)* static_cast<T>(x) + static_cast<T>(y)* static_cast<T>(y) + static_cast<T>(z)* static_cast<T>(z)); }
 	};
 
 	template <>
@@ -72,22 +78,83 @@ namespace Math {
 		Vec3(float i_x, float i_y, float i_z) : x(i_x), y(i_y), z(i_z) {}
 
 		Vec3<float> operator + (Vec3<float> const& i_v) const;
+		Vec3<float> operator - (Vec3<float> const& i_v) const;
+		Vec3<float> operator * (Vec3<float> const& i_v) const;
+		Vec3<float> operator / (Vec3<float> const& i_v) const;
 	};
 
-	inline Vec3<float> Vec3<float>::operator+ (Vec3<float> const& i_v) const 
+	template <typename T>
+	class Vec4
 	{
-		Vec3<float> o_v;
+	public:
+		union
+		{
+			struct { T x, y, z, w; };
+			struct { T r, g, b, a; };
+			T ele[4];
+		};
 
-#ifdef  __AVX__
-		o_v.m_vec = _mm_add_ps(m_vec, i_v.m_vec); 		
-#else
-		o_v = Vec3(this->x + i_v.x, this->y + i_v.y, z + i_v.z);
-#endif //  __AVX__
-		return o_v;
-	}
+		//!@ Constructor
+		Vec4() = default;
+		Vec4(T i_x, T i_y, T i_z, T i_w) : x(i_x), y(i_y), z(i_z), w(i_w) {}
+
+		template <typename U> 
+		explicit Vec4(Vec4<U> const& u) : x(static_cast<T>(u.x)), y(static_cast<T>(u.y)), z(static_cast<T>(u.z)), z(static_cast<T>(u.w)) {}
+		explicit Vec4(Vec3<T> const& t) : x(t.x), y(t.y), z(t.z), w(0) {}
+
+		//!@ Operator with constant value
+		friend Vec4 operator + (T t, Vec4 const& u) { Vec4 o_v; o_v.x = t + u.x; o_v.y = t + u.y; o_v.z = t + u.z; o_v.w = t + u.w; return o_v; }
+		friend Vec4 operator - (T t, Vec4 const& u) { Vec4 o_v; o_v.x = t - u.x; o_v.y = t - u.y; o_v.z = t - u.z; o_v.w = t - u.w; return o_v; }
+		friend Vec4 operator * (T t, Vec4 const& u) { Vec4 o_v; o_v.x = t * u.x; o_v.y = t * u.y; o_v.z = t * u.z; o_v.w = t * u.w; return o_v; }
+		Vec4 operator / (T const t)  const { Vec4 o_v; o_v.x = x / t; o_v.y = y / t; o_v.z = z / t; o_v.w = w / t; return o_v; }
+
+		//!@ Binary operators
+		Vec4 operator + (Vec4 const& i_v) const { Vec4 o_v; o_v.x = x + i_v.x; o_v.y = y + i_v.y; o_v.z = z + i_v.z; o_v.w = w + i_v.w; return o_v; }
+		Vec4 operator - (Vec4 const& i_v) const { Vec4 o_v; o_v.x = x - i_v.x; o_v.y = y - i_v.y; o_v.z = z - i_v.z; o_v.w = w - i_v.w; return o_v; }
+		Vec4 operator * (Vec4 const& i_v) const { Vec4 o_v; o_v.x = x * i_v.x; o_v.y = y * i_v.y; o_v.z = z * i_v.z; o_v.w = w * i_v.w; return o_v; }
+		Vec4 operator / (Vec4 const& i_v) const { Vec4 o_v; o_v.x = x / i_v.x; o_v.y = y / i_v.y; o_v.z = z / i_v.z; o_v.w = w / i_v.w; return o_v; }
+
+		//!@ Assignment operators
+		Vec4 const& operator += (Vec4 const& i_v) { x += i_v.x; y += i_v.y; z += i_v.z; w += i_v.w; return *this; }
+		Vec4 const& operator -= (Vec4 const& i_v) { x -= i_v.x; y -= i_v.y; z -= i_v.z; w -= i_v.w; return *this; }
+		Vec4 const& operator *= (Vec4 const& i_v) { x *= i_v.x; y *= i_v.y; z *= i_v.z; w *= i_v.w; return *this; }
+		Vec4 const& operator /= (Vec4 const& i_v) { x /= i_v.x; y /= i_v.y; z /= i_v.z; w /= i_v.w; return *this; }
+		Vec4 const& operator += (T    const  t) { x += t; y += t; z += t; w += t; return *this; }
+		Vec4 const& operator -= (T    const  t) { x -= t; y -= t; z -= t; w -= t; return *this; }
+		Vec4 const& operator *= (T    const  t) { x *= t; y *= t; z *= t; w *= t; return *this; }
+		Vec4 const& operator /= (T    const  t) { x /= t; y /= t; z /= t; w /= t; return *this; }
+
+		bool operator == (Vec4 const& i_v) const { return x == i_v.x && y == i_v.y && z == i_v.z && w == i_v.w; }
+		bool operator != (Vec4 const& i_v) const { return x != i_v.x && y != i_v.y && z != i_v.z && w != i_v.w; }
+
+		void Zero() { x = 0; y = 0; z = 0; w = 0; }
+		bool IsZero() const { return x == static_cast<T>(0) && y == static_cast<T>(0) && z == static_cast<T>(0) && w == static_cast<T>(0); }
+		void Normalize() { *this /= Length(); }
+		T    Length() const { return sqrt(static_cast<T>(x)* static_cast<T>(x) + static_cast<T>(y)* static_cast<T>(y) + static_cast<T>(z)* static_cast<T>(z) + static_cast<T>(w)* static_cast<T>(w)); }
+	};
+
+	template <>
+	class Vec4<float>
+	{
+	public:
+		union
+		{
+			__m128 m_vec;
+			struct { float x, y, z, w; };
+			struct { float r, g, b, a; };
+		};
+
+		//!@ Constructor
+		Vec4() = default;
+		Vec4(float i_x, float i_y, float i_z, float i_w) : x(i_x), y(i_y), z(i_z), w(i_w) {}
+
+		Vec4<float> operator + (Vec4<float> const& i_v) const;
+		Vec4<float> operator - (Vec4<float> const& i_v) const;
+		Vec4<float> operator * (Vec4<float> const& i_v) const;
+		Vec4<float> operator / (Vec4<float> const& i_v) const;
+	};
 
 } // <- namespace Math
-
 
 //typedef Math::Vec3<unsigned short>   Vec3us;
 //typedef Math::Vec3<short>            Vec3s;
@@ -95,3 +162,6 @@ namespace Math {
 //typedef Math::Vec3<int>              Vec3i;
 typedef Math::Vec3<float>            Vec3f;
 typedef Math::Vec3<double>           Vec3d;
+
+typedef Math::Vec4<float>            Vec4f;
+typedef Math::Vec4<double>           Vec4d;
--- MyGameEngine/Core/Math/Vector.intrinsic.cpp
+++ MyGameEngine/Core/Math/Vector.intrinsic.cpp
@@ -1,71 +1,70 @@
 #include "Vector.h"
 
-#ifndef _USEINTRINSIC_
+#ifdef _USEINTRINSIC_
 
 namespace Math {
 
 	///////////////////////////////////////////////
-	// Vector3<float>	
+	// Vector3<float>
 	///////////////////////////////////////////////
-	Vec3<float> Vec3<float>::operator+ (Vec3<float> const& i_v) const
+	Vec3<float> Vec3<float>::operator+ (Vec3<float> const& i_v) const 
 	{
 		Vec3<float> o_v;
-		o_v = Vec3(this->x + i_v.x, this->y + i_v.y, z + i_v.z);
+		o_v.m_vec = _mm_add_ps(m_vec, i_v.m_vec);
 		return o_v;
 	}
 
 	Vec3<float> Vec3<float>::operator- (Vec3<float> const& i_v) const
 	{
 		Vec3<float> o_v;
-		o_v = Vec3(this->x - i_v.x, this->y - i_v.y, z - i_v.z);
+		o_v.m_vec = _mm_sub_ps(m_vec, i_v.m_vec);
 		return o_v;
 	}
 
 	Vec3<float> Vec3<float>::operator* (Vec3<float> const& i_v) const
 	{
 		Vec3<float> o_v;
-		o_v = Vec3(this->x * i_v.x, this->y * i_v.y, z * i_v.z);
+		o_v.m_vec = _mm_mul_ps(m_vec, i_v.m_vec);
 		return o_v;
 	}
 
 	Vec3<float> Vec3<float>::operator/ (Vec3<float> const& i_v) const
 	{
 		Vec3<float> o_v;
-		o_v = Vec3(this->x / i_v.x, this->y / i_v.y, z / i_v.z);
+		o_v.m_vec = _mm_div_ps(m_vec, i_v.m_vec);
 		return o_v;
 	}
 
 	///////////////////////////////////////////////
 	// Vector4<float>
 	///////////////////////////////////////////////
-
 	Vec4<float> Vec4<float>::operator+ (Vec4<float> const& i_v) const
 	{
 		Vec4<float> o_v;
-		o_v = Vec4(this->x + i_v.x, this->y + i_v.y, z + i_v.z, this->w + i_v.w);
+		o_v.m_vec = _mm_add_ps(m_vec, i_v.m_vec);
 		return o_v;
 	}
 
 	Vec4<float> Vec4<float>::operator- (Vec4<float> const& i_v) const
 	{
 		Vec4<float> o_v;
-		o_v = Vec4(this->x - i_v.x, this->y - i_v.y, z - i_v.z, this->w - i_v.w);
+		o_v.m_vec = _mm_sub_ps(m_vec, i_v.m_vec);
 		return o_v;
 	}
 
 	Vec4<float> Vec4<float>::operator* (Vec4<float> const& i_v) const
 	{
 		Vec4<float> o_v;
-		o_v = Vec4(this->x * i_v.x, this->y * i_v.y, z * i_v.z, this->w * i_v.w);
+		o_v.m_vec = _mm_mul_ps(m_vec, i_v.m_vec);
 		return o_v;
 	}
 
 	Vec4<float> Vec4<float>::operator/ (Vec4<float> const& i_v) const
 	{
 		Vec4<float> o_v;
-		o_v = Vec4(this->x / i_v.x, this->y / i_v.y, z * i_v.z, this->w / i_v.w);
+		o_v.m_vec = _mm_div_ps(m_vec, i_v.m_vec);
 		return o_v;
 	}
 }
 
-#endif // !_USEINTRINSIC_
+#endif // _USEINTRINSIC_
