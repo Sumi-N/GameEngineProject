1ad0641c7534889754ccd25e6e4b703591b6f67b
Sumi <sumi.n.lcy@gmail.com>
2020-01-12 15:54:05 -0700

    Tested multi threading

--- MyGameEngine/Graphics/Graphics/main.cpp
+++ MyGameEngine/Graphics/Graphics/main.cpp
@@ -1,8 +1,33 @@
+#include "Graphic_Vulkan.h"
+#include <thread>
 
-#include "HelloTriangleApplication.h"
+void testThread()
+{
+	//HelloTriangleApplication app;
+
+	//try
+	//{
+	//	app.run();
+	//}
+	//catch (const std::exception & e)
+	//{
+	//	std::cerr << e.what() << std::endl;
+	//	return;
+	//}
+
+	while (1)
+	{
+		//printf("hello world");
+	}
+
+	return;
+}
 
 int main()
 {
+	std::thread th1(testThread);
+	th1.detach();
+
 	HelloTriangleApplication app;
 
 	try
@@ -14,6 +39,8 @@ int main()
 		std::cerr << e.what() << std::endl;
 		return EXIT_FAILURE;
 	}
+
+	//th1.join();
 
 	return EXIT_SUCCESS;
 }
