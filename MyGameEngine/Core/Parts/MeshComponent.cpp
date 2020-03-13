e8cd10668416e1f9a7eb211c118eab265fb1ae87
Sumi <sumi.n.lcy@gmail.com>
2020-03-12 23:02:14 -0600

    Able to show triangles

--- MyGameEngine/Core/Parts/MeshComponent.cpp
+++ MyGameEngine/Core/Parts/MeshComponent.cpp
@@ -1,7 +1,9 @@
 #include "MeshComponent.h"
+#include "../APIs/GraphicAPI/cyCodeBase/cyTriMesh.h"
 
 void MeshComponent::Load(const char* filename)
 {
+	/*
 	tinyobj::attrib_t attrib;
 	std::vector<tinyobj::shape_t> shapes;
 	std::vector<tinyobj::material_t> materials;
@@ -43,6 +45,60 @@ void MeshComponent::Load(const char* filename)
 			this->index.push_back(index.vertex_index);
 		}
 	}
+	*/
+
+	cy::TriMesh tmpdata;
+	tmpdata.LoadFromFileObj(filename, true);
+
+	// Get number of point data
+	int facenum = tmpdata.NF();
+	int vertnum = tmpdata.NV();
+	int normalnum = tmpdata.NVN();
+
+	index.resize(facenum * 3);
+	data.resize(vertnum);
+
+	for (int i = 0; i < vertnum; i++)
+	{
+		// Get normal data
+		data[i].vertex.x = tmpdata.V(i).x;
+		data[i].vertex.y = tmpdata.V(i).y;
+		data[i].vertex.z = tmpdata.V(i).z;
+	}
+
+	for (int i = 0; i < facenum; i++)
+	{
+		// Get index data
+		index[3 * i + 0] = tmpdata.F(i).v[0];
+		index[3 * i + 1] = tmpdata.F(i).v[1];
+		index[3 * i + 2] = tmpdata.F(i).v[2];
+
+		if (tmpdata.NVN() != 0)
+		{
+			// Sort normal to vertex index
+			data[tmpdata.F(i).v[0]].normal.x = tmpdata.VN(tmpdata.FN(i).v[0]).x;
+			data[tmpdata.F(i).v[0]].normal.y = tmpdata.VN(tmpdata.FN(i).v[0]).y;
+			data[tmpdata.F(i).v[0]].normal.z = tmpdata.VN(tmpdata.FN(i).v[0]).z;
+			data[tmpdata.F(i).v[1]].normal.x = tmpdata.VN(tmpdata.FN(i).v[1]).x;
+			data[tmpdata.F(i).v[1]].normal.y = tmpdata.VN(tmpdata.FN(i).v[1]).y;
+			data[tmpdata.F(i).v[1]].normal.z = tmpdata.VN(tmpdata.FN(i).v[1]).z;
+			data[tmpdata.F(i).v[2]].normal.x = tmpdata.VN(tmpdata.FN(i).v[2]).x;
+			data[tmpdata.F(i).v[2]].normal.y = tmpdata.VN(tmpdata.FN(i).v[2]).y;
+			data[tmpdata.F(i).v[2]].normal.z = tmpdata.VN(tmpdata.FN(i).v[2]).z;
+		}
+
+		if (tmpdata.NVT() != 0)
+		{
+			// Sort uv coordinate to vertex index
+			data[tmpdata.F(i).v[0]].uv.x = cy::Point2f(tmpdata.VT(tmpdata.FT(i).v[0])).x;
+			data[tmpdata.F(i).v[0]].uv.y = cy::Point2f(tmpdata.VT(tmpdata.FT(i).v[0])).y;
+			data[tmpdata.F(i).v[1]].uv.x = cy::Point2f(tmpdata.VT(tmpdata.FT(i).v[1])).x;
+			data[tmpdata.F(i).v[1]].uv.y = cy::Point2f(tmpdata.VT(tmpdata.FT(i).v[1])).y;
+			data[tmpdata.F(i).v[2]].uv.x = cy::Point2f(tmpdata.VT(tmpdata.FT(i).v[2])).x;
+			data[tmpdata.F(i).v[2]].uv.y = cy::Point2f(tmpdata.VT(tmpdata.FT(i).v[2])).y;
+		}
+	}
+
 }
 
 void MeshComponent::Init()
