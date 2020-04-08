a6052c46a2554e93b3ecbfa50335485e4aa34917
Sumi <sumi.n.lcy@gmail.com>
2020-04-07 20:27:03 -0600

    cleaned up blinn_shading

--- MyGameEngine/Assets/shaders/blinn_phong_model/blinn_phong.frag.glsl
+++ MyGameEngine/Assets/shaders/blinn_phong_model/blinn_phong.frag.glsl
@@ -1,24 +1,24 @@
 #version 420 core
 
-// Output
-out vec4 color;
-
 // Consta data
 const int   MAX_POINT_LIGHT_NUM = 5;
 const float POINT_LIGHT_BIAS = 0.5;
 const float FAR_PLANE_DISTANCE = 100;
 
+// Output
+out vec4 color;
+
 /////////////////////////////////////////////////////////////////////////////
 
 in VS_OUT{
 	// Object world position
-	vec4 world_object_position;
+	vec4 world_position;
 	// Normal vector of the object at model coordinate
 	vec3 model_normal;
-	// Object direction vector at world coordinate
-	vec3 world_view_direction;
 	// Texture coordinate
 	vec2 texcoord;
+	// Object direction vector at world coordinate
+	vec3 world_view_direction;
 	// Point light direction vector at world coordinate
 	vec3 world_pointlight_direction[MAX_POINT_LIGHT_NUM];
 } fs_in;
@@ -26,9 +26,9 @@ in VS_OUT{
 //////////////////////////////////////////////////////////////////////////////
 // Structure define
 struct PointLight{
-	vec4 point_intensity;
-	vec4 point_position;
-	vec3 point_attenuation;
+	vec4 intensity;
+	vec4 position;
+	vec3 attenuation;
 	float padding;
 };
 //////////////////////////////////////////////////////////////////////////////
@@ -65,11 +65,11 @@ layout(binding = 5) uniform sampler2D texture2; // Normal map
 
 float ShadowCalculation(vec3 fragpos)
 {
-	vec3 fragToLight = fragpos - vec3(pointlights[0].point_position);
-	float closestDepth = texture(shadowmap, fragToLight).r;
+	vec3 fragtolight = fragpos - vec3(pointlights[0].position);
+	float closestDepth = texture(shadowmap, fragtolight).r;
 
 	closestDepth *= FAR_PLANE_DISTANCE;
-	float currentDepth = length(fragToLight);
+	float currentDepth = length(fragtolight);
 
 	float shadow  = 0.0;
 	float samples = 4.0;
@@ -81,7 +81,7 @@ float ShadowCalculation(vec3 fragpos)
 		{
 			for(float z = -offset; z < offset; z += offset / (samples * 0.5))
 			{
-				float closestDepth = texture(shadowmap, fragToLight + vec3(x, y, z)).r; 
+				float closestDepth = texture(shadowmap, fragtolight + vec3(x, y, z)).r; 
 				closestDepth *= FAR_PLANE_DISTANCE;   // Undo mapping [0;1]
 				if(currentDepth - POINT_LIGHT_BIAS > closestDepth)
 					shadow += 1.0;
@@ -94,27 +94,29 @@ float ShadowCalculation(vec3 fragpos)
 
 /////////////////////////////////////////////////////////////////////////////
 
-vec4 CalcPointLightShading(vec3 world_pointlight_direction, vec4 point_intensity, vec3 world_normal, vec3 point_attenuation, vec4 point_position){
 
-	float dist = distance(vec3(point_position), vec3(fs_in.world_object_position));
-	float attenuation = 1.0 / (point_attenuation.x + point_attenuation.y * dist + point_attenuation.z * dist * dist);
-	vec4  radiance = point_intensity * attenuation;
+vec4 CalcPointLightShading(PointLight pointlight, vec3 world_normal, vec3 pointlight_direction){
+//vec4 CalcPointLightShading(vec3 world_pointlight_direction, vec4 intensity, vec3 world_normal, vec3 attenuation, vec4 position){
+
+	float dist = distance(vec3(pointlight.position), vec3(fs_in.world_position));
+	float attenuation = 1.0 / (pointlight.attenuation.x + pointlight.attenuation.y * dist + pointlight.attenuation.z * dist * dist);
+	vec4  radiance = pointlight.intensity * attenuation;
 
 	vec4 color = vec4(0, 0, 0, 1.0);
 
-	float cos_theta_1 = dot(world_normal, world_pointlight_direction);
+	float cos_theta = dot(world_normal, pointlight_direction);
 	
-	if (cos_theta_1 > 0)
+	if (cos_theta > 0)
 	{
-		color += texture2D(texture1,  vec2(fs_in.texcoord.s, 1.0 - fs_in.texcoord.t))  * abs(cos_theta_1) * diffuse * radiance;
+		color += texture2D(texture1,  vec2(fs_in.texcoord.s, 1.0 - fs_in.texcoord.t))  * abs(cos_theta) * diffuse * radiance;
 	
-		vec3 h = normalize(fs_in.world_view_direction + world_pointlight_direction);
+		vec3 half_vec = normalize(fs_in.world_view_direction + pointlight_direction);
 
-		if (dot(h, world_normal) > 0)
+		if (dot(half_vec, world_normal) > 0)
 		{
 			vec3 reflection = -1 * fs_in.world_view_direction + 2 * dot(fs_in.world_view_direction, world_normal) * world_normal;
 
-			color +=  (texture2D(texture1,  vec2(fs_in.texcoord.s, 1.0 - fs_in.texcoord.t)) + texture(skybox, reflection)) * vec4(vec3(radiance) * vec3(specular) * pow(dot(h, world_normal), specular.w), 1.0);
+			color +=  (texture2D(texture1,  vec2(fs_in.texcoord.s, 1.0 - fs_in.texcoord.t)) + texture(skybox, reflection)) * vec4(vec3(radiance) * vec3(specular) * pow(dot(half_vec, world_normal), specular.w), 1.0);
 		}
 	}
 
@@ -125,21 +127,21 @@ vec4 CalcPointLightShading(vec3 world_pointlight_direction, vec4 point_intensity
 /////////////////////////////////////////////////////////////////////////////
 void main()
 {
+	// Calculate world normal of the object
 	vec3 world_normal =  normalize(mat3(model_inverse_transpose_matrix) * fs_in.model_normal);
 	// Ambient light
 	color =texture2D(texture0,  vec2(fs_in.texcoord.s, 1.0 - fs_in.texcoord.t)) * diffuse * ambient_intensity;
 
 	float shadow = 0;
-	for(int i = 0; i < 1; i++){
-		shadow += ShadowCalculation(vec3(fs_in.world_object_position));
-	}
 
-	if(shadow > 1){
-		shadow = 1;
+	for(int i = 0; i < 1; i++){
+		shadow += ShadowCalculation(vec3(fs_in.world_position));
 	}
+	
+	shadow = min(shadow, 1.0);
 
 
 	for(int i = 0; i < point_num; i++){
-		color += CalcPointLightShading(fs_in.world_pointlight_direction[i], pointlights[i].point_intensity, world_normal, pointlights[i].point_attenuation, pointlights[i].point_position) * (1.0 - shadow);
+		color += CalcPointLightShading(pointlights[i], world_normal, fs_in.world_pointlight_direction[i]) * (1.0 - shadow);
 	}
 }
--- MyGameEngine/Assets/shaders/blinn_phong_model/blinn_phong.vert.glsl
+++ MyGameEngine/Assets/shaders/blinn_phong_model/blinn_phong.vert.glsl
@@ -1,21 +1,20 @@
 #version 420 core
 
+// Const data
+const int MAX_POINT_LIGHT_NUM = 5;
+const int CUBE_FACE_NUM = 6;
+
 // Input
 layout (location = 0) in vec3 model_position;
 layout (location = 1) in vec3 model_normal;
 layout (location = 2) in vec2 model_texcoord;
 
-// Const data
-const int MAX_POINT_LIGHT_NUM = 5;
-const int CUBE_FACE_NUM = 6;
-
-
 // Structure define
 //////////////////////////////////////////////////////////////////////////////
 struct PointLight{
-	vec4 point_intensity;
-	vec4 point_position;
-	vec3 point_attenuation;
+	vec4 intensity;
+	vec4 position;
+	vec3 attenuation;
 	float padding;
 };
 //////////////////////////////////////////////////////////////////////////////
@@ -52,13 +51,13 @@ layout (std140, binding = 5) uniform const_shadow
 
 out VS_OUT{
 	// Object world position
-	vec4 world_object_position;
+	vec4 world_position;
 	// Normal vector of the object at model coordinate
 	vec3 model_normal;
-	// Object direction vector at world coordinate
-	vec3 world_view_direction;
 	// Texture coordinate
 	vec2 texcoord;
+	// Object direction vector at world coordinate
+	vec3 world_view_direction;
 	// Point light direction vector at world coordinate
 	vec3 world_pointlight_direction[MAX_POINT_LIGHT_NUM];
 } vs_out;
@@ -66,14 +65,14 @@ out VS_OUT{
 /////////////////////////////////////////////////////////////////////////////
 void main()
 {
-	vs_out.world_object_position             = model_position_matrix * vec4(model_position, 1.0);
+	vs_out.world_position                    = model_position_matrix * vec4(model_position, 1.0);
 	// Send position data at perspective coordinate
 	gl_Position                              = model_view_perspective_matrix * vec4(model_position, 1.0);
 	// Get normal vector at world coordinate
 	vs_out.model_normal                      = model_normal;
 
 	for(int i = 0; i <= point_num; i++){
-		vs_out.world_pointlight_direction[i] = normalize(vec3(pointlights[i].point_position) - vec3(model_position_matrix * vec4(model_position, 1)));
+		vs_out.world_pointlight_direction[i] = normalize(vec3(pointlights[i].position) - vec3(model_position_matrix * vec4(model_position, 1)));
 	}
 
 	vs_out.world_view_direction              = normalize(camera_position_vector -  vec3(model_position_matrix * vec4(model_position, 1)));
